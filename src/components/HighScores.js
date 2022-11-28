3e98ac22aba3b10c1471a2650c9ea34b53b11085
Craig Lawyer <cjl1197@gmail.com>
2022-11-28 11:23:49 -0500

    back to http

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -9,7 +9,7 @@ function Scores() {
   }, [])
 
   const getScores = () => {
-        axios.get('https://lawyernodeserver.duckdns.org:8080/api')
+        axios.get('http://lawyernodeserver.duckdns.org:8080/api')
         .then((response) => {
           const data = response.data
           setScores2(data)
